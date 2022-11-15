8db66daef06e048bdc33bcdd470a77328ac6ef56
Daniel-TheProgrammer <njid18753@gmail.com>
2022-11-15 05:57:56 +0100

    More on Variables and typeof operator

--- VideoOne/script.js
+++ VideoOne/script.js
@@ -1,7 +1,17 @@
 // alert("Hello world!");
 //Variable Declaration
-let Daniel = "cool";
-if (Daniel === "cool") alert("Daniel is a Programmer");
+// let Daniel = "cool";
+// if (Daniel === "cool") alert("Daniel is a Programmer");
 
 //Values and Variables
-console.log("Daniel");
+// console.log("Daniel");
+
+// let firstName = "Nji";
+// let secondName = "Daniel";
+
+// console.log(firstName);
+
+let programmingIsCool = true;
+console.log(programmingIsCool);
+
+console.log(typeof programmingIsCool );
