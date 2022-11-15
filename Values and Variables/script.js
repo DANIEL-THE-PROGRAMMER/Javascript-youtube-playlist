1c4449fdcf3d84ddc3c50b840b5db0724b515363
Daniel-TheProgrammer <njid18753@gmail.com>
2022-11-15 06:44:20 +0100

    arithmetic operators

---
+++ Values and Variables/script.js
@@ -0,0 +1,51 @@
+// alert("Hello world!");
+//Variable Declaration
+// let Daniel = "cool";
+// if (Daniel === "cool") alert("Daniel is a Programmer");
+
+//Values and Variables
+// console.log("Daniel");
+
+// let firstName = "Nji";
+// let secondName = "Daniel";
+
+// console.log(firstName);
+
+// let programmingIsCool = true;
+// console.log(programmingIsCool);
+
+// console.log(typeof programmingIsCool );
+// programmingIsCool = "YES";
+
+// let myNumber = '500'; // oops, this is still a string
+// typeof myNumber;
+
+// const PI = 3.14;
+
+let age = 25;
+age = 31;
+
+console.log(age);
+
+//Basic Operators
+const now = 35;
+const ageDaniel = now - 2018;
+const ageVictor = now - 2018;
+
+console.log(ageDaniel / 2 , ageVictor / 2 ** 3 );
+
+// Math Operators
+const firstName = "Daniel";
+const lastName = "Kevin";
+//Concatenation of strings
+
+console.log(firstName + " " + lastName);
+
+//Assignment Operators
+let x = 11 + 5; //16
+x += 10; // x = x + 10 = 26
+x *= 3; // x = x * 3 = 
+x++;
+x--;
+
+console.log(x);
