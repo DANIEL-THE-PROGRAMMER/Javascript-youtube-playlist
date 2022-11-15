3a1e9f5c59f9980854fb97306206a10f602dcf83
Daniel-TheProgrammer <njid18753@gmail.com>
2022-11-15 05:42:42 +0100

    video 2: Values And Variables

--- VideoOne/script.js
+++ VideoOne/script.js
@@ -1,1 +1,7 @@
-alert("Hello world!");
+// alert("Hello world!");
+//Variable Declaration
+let Daniel = "cool";
+if (Daniel === "cool") alert("Daniel is a Programmer");
+
+//Values and Variables
+console.log("Daniel");
