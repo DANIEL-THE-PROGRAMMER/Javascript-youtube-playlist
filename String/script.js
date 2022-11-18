9fd4b10d2c3f034e86e9ef800645ef3edfdc6ffa
Daniel-TheProgrammer <njid18753@gmail.com>
2022-11-18 15:25:38 +0100

    if/ else control structure

--- String/script.js
+++ String/script.js
@@ -1,16 +1,41 @@
 //Sring and Template literals
-const firstName = "NJI";
-const job = "Software Engineer";
-const birthYear = "1999";
-const year = "2000";
+// const firstName = "NJI";
+// const job = "Software Engineer";
+// const birthYear = "1999";
+// const year = "2000";
 
-const nji = "I'm " + firstName + " am " + (year - birthYear) + " years old " + job + "!";
+// const nji = "I'm " + firstName + " am " + (year - birthYear) + " years old " + job + "!";
 
-console.log(nji);
+// console.log(nji);
 
-const danNew = `I'm ${firstName}, a ${job} and i am ${year - birthYear} years old`
-console.log(danNew);
+// const danNew = `I'm ${firstName}, a ${job} and i am ${year - birthYear} years old`
+// console.log(danNew);
 
-console.log(` Daniel
-The 
-Programmer`);
+// console.log(` Daniel
+// The 
+// Programmer`);
+
+//If/ Else
+const age = 14;
+const isOldEnough = age >= 18;
+console.log(isOldEnough);
+
+if(isOldEnough) {
+    console.log("Daniel can start coding");
+} else {
+    const yearsleft = age - 5;
+    console.log(`Daniel is too young, Wait another ${yearsleft} years :`);
+    console.log("Daniel cannot start coding");
+}
+
+const birthYear = 1999;
+let century;
+
+if (birthYear <= 2000) {
+    century = 20;
+    console.log(`He is old`);
+
+} else {
+    century = 21;
+    console.log(`He is young`);
+}
